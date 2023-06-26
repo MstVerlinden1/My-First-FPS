0a10ae33bee5410c9f8a367999399c6db0998738
Mstverlinden <Mstverlinden@gmail.com>
2023-06-26 03:24:35 +0200

    Weapon object update
    https://youtu.be/Itu3fLoaMP8
    weapons can now be picked up and become a child of where weapons will sit in a player.
    problems that should be worked on are in the trello

--- Game/Assets/Scripts/Weapon.cs
+++ Game/Assets/Scripts/Weapon.cs
@@ -3,22 +3,35 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     [SerializeField] private Collider player;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private KeyCode dropKey = KeyCode.G;
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<Collider>();
+        rb = GetComponent<Rigidbody>();
         Physics.IgnoreCollision(player.GetComponent<Collider>(), GetComponent<Collider>());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        DropWeapon();
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 6)
         {
-            gameObject.transform.parent = other.transform;
+            gameObject.transform.SetParent(GameObject.Find("Weapons").transform);
+            gameObject.transform.localPosition = Vector3.zero;
+            rb.isKinematic= true;
+        }
+    }
+    private void DropWeapon()
+    {
+        if (Input.GetKey(dropKey))
+        {
+            gameObject.transform.parent = null;
+            rb.isKinematic= false;
         }
     }
 }
